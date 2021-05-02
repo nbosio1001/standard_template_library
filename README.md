6f5ca6cbd554a3801a04c31d26bf6a0188224ddd
nbosio1001 <n.bosio@yahoo.com>
2021-05-02 12:29:21 -0700

    Descending and ascending algorithms work

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # standard_template_library
+Editing geeksforgeeks tutorial on Standard template library (https://www.geeksforgeeks.org/the-c-standard-template-library-stl/)
--- algorithms/non_manipulating/sort.cpp
+++ algorithms/non_manipulating/sort.cpp
@@ -1,47 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int arr[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
-    int n = sizeof(arr) / sizeof(arr[0]); // n=10, sizeof(arr)=40, sizeof(arr[0])=4
-    // 4 bits per integer
-    int asceding_sort(int arr, int n);
-    int descending_sort(int arr, int n);
-    
-    // cout << "Array after sorting : \n";
+void show(int arr[], int n){
     for (int i = 0; i < n; ++i)
         cout << arr[i] << " ";
+}
+
+int main(){
+    int arr[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 }; // size of array always 10
+    int n = sizeof(arr) / sizeof(arr[0]); // n=10, sizeof(arr)=40, sizeof(arr[0])=4
+    
+    cout << "The array before sorting is : \n";
+    
+    // Print array
+    show(arr, n);
+    
+    // Asceding Sort
+    sort(arr, arr + n);
+    // int asc_arr[] = sort(arr, arr + n);
+
+    cout << "\n Array after sorting using "
+        "default sort is : \n";
+    show(arr, n);
+
+    // Descending Sort
+    sort(arr, arr + n, greater<int>());
+    
+    cout << "\n Array after descending sorting : \n";
+    show(arr, n);
 
     return 0;
-}
-
-int asceding_sort(int arr[], int n)
-{ 
-    /*Here we take two parameters, the beginning of the
-    array and the length n upto which we want the array to
-    be sorted*/
-    sort(arr, arr + n);
-  
-    cout << "\nArray after sorting using "
-            "default sort is : \n";
-    // for (int i = 0; i < n; ++i)
-    //     cout << arr[i] << " ";
-  
-    // return 0;
-}
-
-// C++ program to demonstrate descending order sort using
-// greater<>().
-int descending_sort(int arr[], int n)
-{
-  
-    sort(arr, arr + n, greater<int>());
-  
-    cout << "Array after sorting : \n";
-    // for (int i = 0; i < n; ++i)
-    //     cout << arr[i] << " ";
-  
-    // return 0;
 }
 
 
